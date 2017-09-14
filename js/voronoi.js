fdc55bb08e1f8da81a3e0934f9f115abc7a83345
Vinícius Aguiar <viniaguiar18@gmail.com>
2017-09-13 23:17:03 -0300

    trip unstable

--- js/voronoi.js
+++ js/voronoi.js
@@ -4,8 +4,8 @@ var svg = d3.select("svg"), voronoi,
           site, defaultTheme, changeColor = false;
 
 var params = {
-  points: 42,
-  type: "diagram",
+  points: 6,
+  type: "trip",
   smoothness: smoothBorders,
   colorfulness: function(){
     changeColor = true;
@@ -16,27 +16,50 @@ var params = {
 var gui = new dat.GUI(),
     typeControl = gui.add(params, "type",
       ["trip", "diagram", "drag and drop"]),
-    pointsControl = gui.add(params, "points").min(1).max(256).step(1);
+    pointsControl = gui.add(params, "points").min(1).max(64).step(1);
  gui.add(params, "smoothness");
  gui.add(params, "colorfulness");
 
 // event listeners.............................................................
 window.onresize = resized;
-typeControl.onFinishChange(typeOf);
-pointsControl.onChange(newVoronoi);
+typeControl.onFinishChange(typeChanged);
+pointsControl.onFinishChange(newVoronoi);
 
 // initial state...............................................................
 newVoronoi();
 
-// functions...................................................................
-function moved() {
-  sites[0] = d3.mouse(this);
-  redraw();
+// type and size functions.....................................................
+function typeChanged(value){
+  var circles = d3.selectAll("circle");
+
+  svg.on("touchmove mousemove", null);
+
+  if(value == "drag and drop"){
+    circles.call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended))
+          .transition().attr("r", 10);
+  }
+  else {
+    circles.call(d3.drag()
+          .on("start", null)
+          .on("drag", null)
+          .on("end", null));
+    if(value == "diagram"){
+      svg.on("touchmove mousemove", moved);
+      circles.transition().attr("r", 3);
+    }
+    else if(value == "trip"){
+      circles.transition().attr("r", 6);
+      trip();
+    }
+  }
 }
 
-function resized() {
-	var width = window.innerWidth,
-			height = window.innerHeight;
+function resized(){
+  var width = window.innerWidth,
+      height = window.innerHeight;
 
   svg
     .attr("width", width)
@@ -48,9 +71,49 @@ function resized() {
   sites = points
     .map(function(d) { return [d[0] * width, d[1] * height]; });
 
+  console.log(d3.selectAll("circle").data());
   redraw();
 }
 
+// trip functions..............................................................
+function trip(){
+  //svg.selectAll("*").remove();
+
+  d3.timer(function(elapsed){
+    var circles = svg.selectAll("circle");
+    sites = [];
+
+    circles.each(function(){
+      var circle = d3.select(this),
+          speedx = parseFloat(circle.attr("speed.x")),
+          speedy = parseFloat(circle.attr("speed.y")),
+          cx = parseFloat(circle.attr("cx")) + speedx;
+          cy = parseFloat(circle.attr("cy")) + speedy;
+
+      if(cx <= 0 || cx >= svg.attr("width"))
+        circle.attr("speed.x", -1*speedx);
+      circle.attr("cx", cx);
+
+      if(cy <= 0 || cy >= svg.attr("height"))
+        circle.attr("speed.y", -1*speedy);
+      circle.attr("cy", cy);
+
+      sites.push([circle.attr("cx"), circle.attr("cy")]);
+    });
+
+    redraw();
+  });
+
+  //redraw();
+}
+
+// diagram function............................................................
+function moved(){
+  sites[0] = d3.mouse(this);
+  redraw();
+}
+
+// drag and drop functions.....................................................
 function dragstarted(){
 	d3.select(this).classed("active", true);
 }
@@ -80,33 +143,7 @@ function dragended(){
   d3.select(this).classed("active", false);
 }
 
-function typeOf(value){
-  var circle = d3.selectAll("circle");
-
-  svg.on("touchmove mousemove", null);
-
-  if(value == "drag and drop"){
-    circle.call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended))
-          .transition().attr("r", 10);
-  }
-  else {
-    circle.on("start", null)
-          .on("drag", null)
-          .on("end", null);
-    if(value == "diagram"){
-      svg.on("touchmove mousemove", moved);
-      circle.transition().attr("r", 3);
-    }
-    else {
-      circle.transition().attr("r", 6);
-    }
-  }
-}
-
-// basic functions ............................................................
+// essential functions.........................................................
 function newVoronoi(){
   var width = window.innerWidth,
 		  height = window.innerHeight,
@@ -147,9 +184,11 @@ function newVoronoi(){
 	  .data(sites)
 	  .enter().append("circle")
 	    .attr("r", radius)
+      .attr("speed.x", 3*Math.random())
+      .attr("speed.y", 3*Math.random())
 	    .call(redrawSite);
   
-  typeOf(params.type);
+  typeChanged(params.type);
   smoothColors();
   redraw();
 }
@@ -162,8 +201,6 @@ function smoothColors(){
     ["#3F413A","#515A64","#78A8B1","#9BAEB6","#D6C1A4"],  //the lego movie
     ["#424937","#384037","#8B2F2A","#877D14","#E1B154"]   //paper mario
   ];
-  
-  console.log("paletteLength: " + palette.length);
 
   if(changeColor || defaultTheme == undefined){
     theme = Math.floor(Math.random() * palette.length);
@@ -171,8 +208,6 @@ function smoothColors(){
   }
   else
     theme = defaultTheme;
-
-  console.log("defaultTheme: " + defaultTheme);
   
   d3.selectAll("path").each(function(){
     color = Math.floor(Math.random() * 5);
@@ -186,7 +221,7 @@ function smoothBorders(){
 
 }
 
-function redraw() {
+function redraw(){
   var diagram = voronoi(sites);
   polygon = polygon.data(diagram.polygons()).call(redrawPolygon);
   link = link.data(diagram.links()), link.exit().remove();
@@ -194,12 +229,12 @@ function redraw() {
   site = site.data(sites).call(redrawSite);
 }
 
-function redrawPolygon(polygon) {
+function redrawPolygon(polygon){
   polygon
     .attr("d", function(d) { return d ? "M" + d.join("L") + "Z" : null; });
 }
 
-function redrawLink(link) {
+function redrawLink(link){
   link
       .attr("x1", function(d) { return d.source[0]; })
       .attr("y1", function(d) { return d.source[1]; })
@@ -207,7 +242,7 @@ function redrawLink(link) {
       .attr("y2", function(d) { return d.target[1]; });
 }
 
-function redrawSite(site) {
+function redrawSite(site){
   site
       .attr("cx", function(d) { return d[0]; })
       .attr("cy", function(d) { return d[1]; });
